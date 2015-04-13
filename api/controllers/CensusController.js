781bdea655932c3b56c1bf1d209192147e13f9cd
Steven Heinrich <steven.heinrich@gmail.com>
2015-04-12 21:12:55 -0500

    update URL to grab more fields

--- api/controllers/CensusController.js
+++ api/controllers/CensusController.js
@@ -9,11 +9,9 @@ module.exports = {
 
     getPopulationByState: function(req, res) {
         var request = require('request');
-        request('http://api.census.gov/data/2010/sf1?key=730e49d850365e415931ee4f3a309ffea517b92d&get=P0010001&for=state', 
+        request('http://api.census.gov/data/2010/sf1?key=730e49d850365e415931ee4f3a309ffea517b92d&get=P0010001,P0030001,P0030002,P0030003,P0030004,P0030005,P0030006,P0030007,P0030008&for=state', 
             function (error, response, body) {
               if(!error && response.statusCode === 200) {
-                // res.setHeader('Content-Type', 'application/json');
-                // res.send(JSON.stringify(DataConverter.mapCensusResults(body)));
                 res.json(DataConverter.mapCensusResults(body));
               }
         });
